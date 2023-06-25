702169d968f406e08cd45219d9b5ba45a946326a
bar07865 <137136679+bar07865@users.noreply.github.com>
2023-06-25 05:43:03 +0000

    fix lab again

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 def main():
 
     original_str = "Python Programming"
-    sub1 = original_str[0:6]
+    sub1 = original_str[0:7]
     sub2 = original_str[7:18]
 
-    merge_str = sub1 +" "+ sub2
+    merged_str = sub1 + sub2
 
     print(sub2)
     print(sub1)
-    print(merge_str)
+    print(merged_str)
 
 
 if __name__ == '__main__':
